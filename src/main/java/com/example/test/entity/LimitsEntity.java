0b41642c43c6a5e6119249535e61dc7a31e25731
trus0lya <trus0lya@yandex.by>
2024-02-09 14:34:46 +0300

    modify limits entity

--- src/main/java/com/example/test/entity/LimitsEntity.java
+++ src/main/java/com/example/test/entity/LimitsEntity.java
@@ -21,22 +21,28 @@ public class LimitsEntity {
     @Id
     @Column(name = "id", nullable = false)
     private Long id;
+
     @Basic
     @Column(name = "account_number", nullable = false)
     private Long accountNumber;
+
     @Basic
     @Column(name = "expense_category", nullable = false)
     @Enumerated(EnumType.STRING)
     private ExpenseCategory expenseCategory;
+
     @Basic
     @Column(name = "limit_usd", nullable = false, precision = 2)
     private BigDecimal limitUsd;
+
     @Basic
     @Column(name = "creation_date", nullable = false)
     private Timestamp creationDate;
+
     @Basic
     @Column(name = "remains_before_exceed", nullable = false, precision = 2)
     private BigDecimal remainsBeforeExceed;
+
     @Basic
     @Column(name = "update_date", nullable = false)
     private Timestamp updateDate;
