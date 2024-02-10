c12eac1c27cc2c877d0b8748018b393db40c3e67
trus0lya <trus0lya@yandex.by>
2024-02-10 22:55:12 +0300

    added twelvedata.properties

--- src/main/java/com/example/test/config/AppConfig.java
+++ src/main/java/com/example/test/config/AppConfig.java
@@ -3,9 +3,11 @@ package com.example.test.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("classpath:twelvedata.properties")
 public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
